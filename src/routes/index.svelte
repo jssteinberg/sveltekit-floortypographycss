87b5896b3f6ba82c35edecec83206856f7b7207b
Steinberg <steinberg@hey.com>
2021-07-13 01:36:00 +0200

    Change comma to em-dash

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,5 +4,5 @@
 </script>
 
 <p>
-A guide to setup <a href="//kit.svelte.dev">Sveltekit</a> and <a href="//floortypography.vercel.app">Floor Typography CSS</a>, a postcss library which makes custom text-book typographic CSS easy, and it has updated normalization across browsers.
+A guide to setup <a href="//kit.svelte.dev">Sveltekit</a> and <a href="//floortypography.vercel.app">Floor Typography CSS</a>—a postcss library which makes custom text-book typographic CSS easy, and it has updated normalization across browsers.
 </p>
