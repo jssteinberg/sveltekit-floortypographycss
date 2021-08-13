f5670e2293dfb840da6cbd7e9cd7f595160280c2
Steinberg <steinberg@hey.com>
2021-08-14 00:06:07 +0200

    Change to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import vercel from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 	const config = {
 		kit: {
-			adapter: vercel(),
-			target: '#svelte'
+			target: '#svelte',
+			adapter: adapter({
+				// default options are shown
+				pages: 'build',
+				assets: 'build',
+				fallback: null
+			})
 		},
 
 		preprocess: [preprocess({
