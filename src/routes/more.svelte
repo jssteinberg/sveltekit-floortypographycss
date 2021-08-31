b16286cb1812fcbcbd658af8534094aad415e049
Johan <729055+jssteinberg@users.noreply.github.com>
2021-08-31 09:47:53 +0200

    Update more.svelte

--- src/routes/more.svelte
+++ src/routes/more.svelte
@@ -6,6 +6,12 @@
 <h2>Deploy</h2>
 <p>To deploy, install the relevant <a href="https://kit.svelte.dev/docs#adapters">adapter</a>.</p>
 
+<h2>Prefetch routes on hover/touch</h2>
+
+<p>
+	Even thought not stated in <a href="https://kit.svelte.dev/docs#anchor-options-sveltekit-prefetch">the docs</a> (at the time of writing this), `sveltekit:prefetch` will prefetch a route when the user hovers or touches a link even though there is no load function.
+</p>
+
 <h2>No/less JS</h2>
 
 <p>
